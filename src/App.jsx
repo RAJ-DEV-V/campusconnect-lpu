eb0e909fdf60c961226fc3a749507687412ee4ff
Rajvansh Mishra <mishra.rajvansh11@gmail.com>
2026-07-04 00:51:24 +0530

    Install Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 function App() {
   return (
-    <div>
-      <h1>CampusConnect LPU</h1>
-      <p>Connect. Network. Grow.</p>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="bg-white rounded-2xl shadow-xl p-10 text-center">
+        <h1 className="text-5xl font-bold text-blue-600">
+          CampusConnect LPU
+        </h1>
+
+        <p className="mt-4 text-gray-600 text-lg">
+          Connect. Network. Grow.
+        </p>
+
+        <button className="mt-8 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-xl transition">
+          Get Started
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
